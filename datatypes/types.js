d1259d4bf501c8a3db1b964a5e0ca3cb41a3656d
sadieBoBadie <sflick@codingdojo.com>
2021-05-05 11:38:02 -0700

    W1D3 Morning Code

---
+++ datatypes/types.js
@@ -0,0 +1,40 @@
+// COMMENTS
+/* This is a huge comment for me to read later */
+
+// Datatypes & Variables
+// numbers
+//   variable    value
+//   where        what
+var justinsAge = 18;
+
+// boolean (true/false)
+var isVeteran = false;
+
+// strings
+var name = 'Justin "the man" Aguilo';
+var content = 'This is a super long.'
+
+var ricardosAge = justinsAge;
+
+// undefined
+var personsAge;
+
+// prints the variable or element to the console.
+console.log(personsAge);
+
+
+var currentClient = null;
+
+currentClient = "TK Rivers";
+
+//    true/false
+if (justinsAge < 21) {
+    console.log("Sorry, no booze for you!");
+    console.log("And that's lame!");
+}
+else {
+    console.log("Cool you can buy beer!");
+}
+
+
+
